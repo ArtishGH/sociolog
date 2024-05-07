5d0cac329bb6ac61c909f4e3403fb8a30713be65
ArturPiechal <99868375+ArturPiechal@users.noreply.github.com>
2024-05-08 01:06:58 +0200

    About the problem + start Our offer

--- main/src/components/landing/LandingView/index.tsx
+++ main/src/components/landing/LandingView/index.tsx
@@ -1,18 +1,21 @@
-// import About from "../AboutTheProblem";
+import About from "../AboutTheProblem";
+import OurOffer from "../OurOffer";
 import "./style.scss";
 
 export const LandingView = () => {
   return (
     <div className="view">
       <div className="container">
-          <div className="left-bar">
-            <h1 className="header-title">Let us help you</h1>
-            <h4 className="header-motto">Understand your kid</h4>
-          </div>
-          <div className="right-bar">
-            <img src="../../../public/img/depressedGirl.png" alt="Laughter" />
-          </div>
+        <div className="left-bar">
+          <h1 className="header-title">Let us help you</h1>
+          <h4 className="header-motto">Understand your kid</h4>
+        </div>
+        <div className="right-bar">
+          <img src="../../../public/img/depressedGirl.png" alt="Laughter" />
+        </div>
       </div>
+      <About />
+      <OurOffer />
     </div>
   );
 };
