6921e70cf75ea87b40bd0f5b58972dea22545fc6
PiotrChoinski <97666907+PiotrChoinski@users.noreply.github.com>
2024-05-07 21:21:19 +0200

    working posts
    add post, view posts, add comment

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,10 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import { WebSocketServer } from "ws";
+import { getPosts, addPost, addComment } from "./posts.js";
 
 const app = express();
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
@@ -13,11 +15,24 @@ app.use(express.urlencoded({ extended: true }));
 const server = app.listen(8080, () => console.log("Server is listening on port 8080"));
 const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
+wss.on("connection", async (ws) => {
+  const posts = await getPosts();
+  ws.send(JSON.stringify(posts));
 
   ws.on("message", (message) => {
-    console.log(`Received message => ${message}`);
-    ws.send(`You sent => ${message}`);
+    message = JSON.parse(message);
+    switch (message.type) {
+      case "ADD_POST":
+        addPost(message.content);
+        break;
+      case "ADD_COMMENT":
+        addComment(message.postId, message.comment);
+        break;
+      default:
+        break;
+    }
+    wss.clients.forEach(
+      (client) => client !== ws && client.readyState === ws.OPEN && client.send(JSON.stringify(getPosts()))
+    );
   });
 });
---
+++ server/src/posts.js
@@ -0,0 +1,49 @@
+// export interface Message {
+//   id: number;
+//   sender: string;
+//   content: string;
+// }
+
+// export interface Conversation {
+//   id: number;
+//   creator: string;
+//   messages: Message[];
+// }
+
+const posts = [
+  {
+    id: 1,
+    content: "Hello, World!",
+    comments: [
+      { id: 1, content: "Hello, Bob!" },
+      { id: 2, content: "Hi, Alice!" },
+      { id: 3, content: "How are you?" },
+      { id: 4, content: "I am fine, thank you!" },
+      { id: 5, content: "Good to hear that!" },
+    ],
+  },
+  {
+    id: 2,
+    content: "Hello, World!",
+    comments: [
+      { id: 1, content: "Hello, Alice!" },
+      { id: 2, content: "Hi, Bob!" },
+      { id: 3, content: "How are you?" },
+      { id: 4, content: "I am fine, thank you!" },
+      { id: 5, content: "Good to hear that!" },
+    ],
+  },
+];
+
+export const getPosts = (id) => (id ? posts.find((post) => post.id === id) : posts);
+
+export const addPost = (content) => {
+  const post = { id: posts.slice(-1)[0].id + 1, content, comments: [] };
+  posts.push(post);
+};
+
+export const addComment = (postId, comment) => {
+  const post = posts.find((post) => post.id === parseInt(postId));
+  comment = { id: post.comments.slice(-1)[0].id + 1, ...comment };
+  post.comments.push(comment);
+};
