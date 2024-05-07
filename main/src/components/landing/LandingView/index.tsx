5f7a453c094365945c508e73d99bf1152695ee44
Kacper Śmielak <kacper.smielak@wp.pl>
2024-05-07 22:56:07 +0200

    correct into frontend

--- main/src/components/landing/LandingView/index.tsx
+++ main/src/components/landing/LandingView/index.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import About from "../AboutTheProblem";
+// import About from "../AboutTheProblem";
 import "./style.scss";
 
 export const LandingView = () => {
   return (
-    <div className="landing-container">
-      <header className="landing-header">
-        <div className="header-content">
-          <h1 className="header-title">Let us help you</h1>
-          <h4 className="header-motto">Understand your kid</h4>
-        </div>
-        <div className="header-image">
-          <img src="../../../public/img/depressedGirl.png" alt="Laughter" />
-        </div>
-      </header>
-      <About />
+    <div className="view">
+      <div className="container">
+          <div className="left-bar">
+            <h1 className="header-title">Let us help you</h1>
+            <h4 className="header-motto">Understand your kid</h4>
+          </div>
+          <div className="right-bar">
+            <img src="../../../public/img/depressedGirl.png" alt="Laughter" />
+          </div>
+      </div>
     </div>
   );
 };
-
